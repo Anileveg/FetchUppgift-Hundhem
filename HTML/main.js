5e963403d15b44f1003f1aa001687130ff435ea8
EVELINA <evelinaghazzaoui@hotmail.com>
2022-03-02 23:14:11 +0100

    Allt klart för att skickas in

--- HTML/main.js
+++ HTML/main.js
@@ -1,32 +1,31 @@
-function fetchData(){
 
-    let knapp1 = document.getElementById('knapp1')
-    let bild1 = document.getElementById('bild1');
+
+     let knapp1 = document.getElementById('knapp1')
+     let bild1 = document.getElementById('bild1');
     
-    knapp1.addEventListener('click', function(){
+     knapp1.addEventListener('click', function(){
+
         fetch('https://dog.ceo/api/breed/hound/images')
         .then(response => response.json())
-        .then(result => {
-         bild1.src = result.message[0] 
-         bild2.src = result.message[1] 
-         bild3.src = result.message[2] 
-         bild4.src = result.message[3] 
-         bild5.src = result.message[4] 
-         bild6.src = result.message[5] 
-         bild7.src = result.message[6] 
-         bild8.src = result.message[7] 
-         bild9.src = result.message[8] 
-         bild10.src = result.message[9] 
+        .then(data => {
+         bild1.src = data.message[0] 
+         bild2.src = data.message[1] 
+         bild3.src = data.message[2] 
+         bild4.src = data.message[3] 
+         bild5.src = data.message[4] 
+         bild6.src = data.message[5] 
+         bild7.src = data.message[6] 
+         bild8.src = data.message[7] 
+         bild9.src = data.message[8] 
+         bild10.src= data.message[9] 
 
          console.log(result)
-
-         
     })
     .catch(error=>console.log (error))
     
-    })
-
-    let slumpa = document.getElementById('slumpa')
+    })  
+ 
+     let slumpa = document.getElementById('slumpa')
     let slumpbild = document.getElementById('slumpbild');
     
     slumpa.addEventListener('click', function(){
@@ -43,6 +42,8 @@ function fetchData(){
     .catch(error=>console.log (error))
     
     })
-    }
+     
 
-    fetchData();
+
+
+    
